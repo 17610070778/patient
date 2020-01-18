57c8716ff38fbc9dc3b7ccb589735bfa32b21ffb
17610070778 <38308694+17610070778@users.noreply.github.com>
2020-01-18 19:20:18 +0800

    Update index.vue

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,15 +1,17 @@
 <template>
   <div>登录页</div>
 </template>
-
+这是冲突
 <script>
+这是冲突
 export default {
   data () {
     return {}
   },
   components: {}
 }
+这是冲突
 </script>
-
+这是冲突
 <style>
 </style>
